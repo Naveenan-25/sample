9ff8702a152c65c54a511de63e4eaac5c0595793
Naveen25 <98873140+Naveenan-25@users.noreply.github.com>
2022-04-19 14:23:52 +0530

    Create control_switch.cpp

---
+++ 3_Implementations/SRC/control_switch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+enum Appliance{
+            Fan = 1,
+            Light,
+            Fridge,
+
+        };
+ 
+ class smartswitch
+ { 
+     public:
+     int value;
+     int x;
+     int Choice;
+     int status;
+     int intensity;
+     string Level
