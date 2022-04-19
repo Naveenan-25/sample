c3b911d078290c7ff6b42045cd2fbbcd37957435
Naveen25 <98873140+Naveenan-25@users.noreply.github.com>
2022-04-19 14:21:27 +0530

    Create user.h

---
+++ 3_Implementations/INC/user.h
@@ -0,0 +1,15 @@
+#ifndef CONTROL_H_
+#define CONTROL_H_
+
+int bulb_control();
+int fan_control();
+int fridge_control();
+int option();
+/*class display:smart_switch
+{
+    public:
+        void displaybulb();
+        void displayfan();
+        void displayfridge();
+};*/
+#endif
